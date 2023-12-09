dce938d933d7d235457c7699d0e94e8ac4cad68b
GarrettShrop <typicalgamer456@gmail.com>
2023-12-08 23:12:07 -0800

    adding update and testing git push account

--- backend/src/profile/dto/create-profile.input.ts
+++ backend/src/profile/dto/create-profile.input.ts
@@ -1,4 +1,4 @@
-import { InputType, Field } from '@nestjs/graphql';
+import { InputType, Field, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateProfileInput {
@@ -19,4 +19,28 @@ export class CreateProfileInput {
 
   @Field()
   portfolioLink: string;
+}
+
+@InputType()
+export class UpdateProfileInput {
+  @Field(() => Int)
+  id: number; // You'll need the ID to identify which profile to update
+
+  @Field({ nullable: true })
+  name?: string;
+
+  @Field({ nullable: true })
+  phoneNumber?: string;
+
+  @Field({ nullable: true })
+  email?: string;
+
+  @Field({ nullable: true })
+  githubLink?: string;
+
+  @Field({ nullable: true })
+  linkedinLink?: string;
+
+  @Field({ nullable: true })
+  portfolioLink?: string;
 }
